5b9267d7424e3e0582e3364c787eef048a74263a
Tubagus Gita Priadi <66976191+merdekadwiputra@users.noreply.github.com>
2020-08-12 11:55:48 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,9 +2,9 @@
 //error_reporting(E_ALL); ini_set('display_errors', 1);
 //mysqli_report(MYSQLI_REPORT_ERROR);
 
-$host = "merdeka.hopto.org";
-$user = "tubagus";
-$pass = "kontolkuda";
+$host = "localhost";
+$user = "root";
+$pass = "";
 $database = "db_honda";
 
 $db = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
